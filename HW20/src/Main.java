ab3317b3b145b8f8585d3830acc8fad848c7eb27
Whiteburn2023 <erunda995@yandex.ru>
2024-10-09 17:21:10 +0500

    ready

--- HW20/src/Main.java
+++ HW20/src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Musician music = new Musician();
+        Piano piano = new Piano();
+        Guitar guitar = new Guitar();
+        Flute flute = new Flute();
+
+        music.play(piano);
+        music.play(guitar);
+        music.play(flute);
     }
 }
